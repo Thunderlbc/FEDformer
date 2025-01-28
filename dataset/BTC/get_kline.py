e50abc96d4b22efee7c6276b8278ade30f3afa81
luka <thunderlbc@gmail.com>
2025-01-28 17:02:56 +0800

    fxi

--- dataset/BTC/get_kline.py
+++ dataset/BTC/get_kline.py
@@ -44,31 +44,60 @@ def get_binance_data(symbol, start_str, end_str, interval):
 timeframes = ['1m', '3m', '5m', '15m', '30m', '1h', '2h', '4h', '6h', '8h', '12h', '1d']
 
 # Retrieving and saving data for each timeframe
+#for timeframe in timeframes:
+#    data = get_binance_data('BTCUSDT', '2025-01-01', '2025-01-21', timeframe)
+#    filename = f'btcusdt_{timeframe}_2025.csv'
+#
+#    with open(filename, 'w', newline='') as file:
+#        writer = csv.writer(file)
+#        # Add headers
+#        headers = [
+#            'Open_time',
+#            'Open_price',
+#            'High_price',
+#            'Low_price',
+#            'Close_price',
+#            'Volume',
+#            'Close_time',
+#            'Quote_asset_volume',
+#            'Number_of_trades',
+#            'Taker_buy_base_volume',
+#            'Taker_buy_quote_volume',
+#            'Ignore'
+#        ]
+#        writer.writerow(headers)
+#        for row in data:
+#            writer.writerow(row)
+#    
+#    print(f"Data saved in {filename}")
+#
+#print("All data has been successfully collected and saved.")
+
+"""
+BTCUSDT_1m_2025.csv文件包含了2025年1月1日到2025年1月21日的BTCUSDT的1分钟K线数据
+     他的每一列分别是： Open_time, Open_price, High_price, Low_price, Close_price, Volume, Close_time, Quote_asset_volume, Number_of_trades, Taker_buy_base_volume, Taker_buy_quote_volume, Ignork
+    基于这些数据，我们生成：
+    1. delta_open_price_ratio: 开盘价与前一个开盘价的差值比
+    2. delta_high_price_ratio: 最高价与前一个最高价的差值比
+    3. delta_low_price_ratio: 最低价与前一个最低价的差值比
+    4. delta_close_price_ratio: 收盘价与前一个收盘价的差值比
+    5. delta_volume_ratio: 成交量与前一个成交量的差值比
+    6. delta_quote_asset_volume_ratio: 成交额与前一个成交额的差值比
+    7. delta_number_of_trades_ratio: 成交笔数与前一个成交笔数的差值比
+    8. delta_taker_buy_base_volume_ratio: 主动买入成交量与前一个主动买入成交量的差值比
+    9. delta_taker_buy_quote_volume_ratio: 主动买入成交额与前一个主动买入成交额的差值比
+"""
+import pandas as pd
 for timeframe in timeframes:
-    data = get_binance_data('BTCUSDT', '2025-01-01', '2025-01-21', timeframe)
     filename = f'btcusdt_{timeframe}_2025.csv'
-
-    with open(filename, 'w', newline='') as file:
-        writer = csv.writer(file)
-        # Add headers
-        headers = [
-            'Open_time',
-            'Open_price',
-            'High_price',
-            'Low_price',
-            'Close_price',
-            'Volume',
-            'Close_time',
-            'Quote_asset_volume',
-            'Number_of_trades',
-            'Taker_buy_base_volume',
-            'Taker_buy_quote_volume',
-            'Ignore'
-        ]
-        writer.writerow(headers)
-        for row in data:
-            writer.writerow(row)
-    
-    print(f"Data saved in {filename}")
-
-print("All data has been successfully collected and saved.")
+    df = pd.read_csv(filename)
+    df['delta_open_price_ratio'] = df['Open_price'].diff() / df['Open_price'].shift(1)
+    df['delta_high_price_ratio'] = df['High_price'].diff() / df['High_price'].shift(1)
+    df['delta_low_price_ratio'] = df['Low_price'].diff() / df['Low_price'].shift(1)
+    df['delta_close_price_ratio'] = df['Close_price'].diff() / df['Close_price'].shift(1)
+    df['delta_volume_ratio'] = df['Volume'].diff() / df['Volume'].shift(1)
+    df['delta_quote_asset_volume_ratio'] = df['Quote_asset_volume'].diff() / df['Quote_asset_volume'].shift(1)
+    df['delta_number_of_trades_ratio'] = df['Number_of_trades'].diff() / df['Number_of_trades'].shift(1)
+    df['delta_taker_buy_base_volume_ratio'] = df['Taker_buy_base_volume'].diff() / df['Taker_buy_base_volume'].shift(1)
+    df['delta_taker_buy_quote_volume_ratio'] = df['Taker_buy_quote_volume'].diff() / df['Taker_buy_quote_volume'].shift(1)
+    df.to_csv(f'btcusdt_{timeframe}_2025_delta.csv', index=False)
