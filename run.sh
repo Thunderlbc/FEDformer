b1993185ef435b1b8461cff3ffe3c7e69ec52832
luka <thunderlbc@gmail.com>
2025-01-31 11:14:53 +0800

    fix

--- run.sh
+++ run.sh
@@ -13,4 +13,4 @@
 #done
 #
 
-python run.py --data BTCUSDT_train --model FEDformer --version Fourier --data_path btcusdt_1m_2025_delta.csv --root_path ./dataset/BTC/ --features MS --target delta_close_price_ratio --seq_len 200 --label_len 20 --pred_len 1
+python run.py --data BTCUSDT_train --model FEDformer --version Fourier --data_path btcusdt_1m_2025_delta.csv --root_path ./dataset/BTC/ --features MS --target delta_close_price_ratio --seq_len 200 --label_len 100 --pred_len 1 --enc_in 9 --dec_in 9 --c_out 9 --use_multi_gpu true --devices 1,2,3
